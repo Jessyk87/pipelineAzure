f4a71a95f967feb769894fe63ec6f741e306d363
Pedro Tenezaca <pedroalex.tenezaca@gmail.com>
2022-04-10 01:35:08 -0500

    Correción codigo en base a pruebas

--- src/main/java/com/bp/msdevops/rest/DevOpsController.java
+++ src/main/java/com/bp/msdevops/rest/DevOpsController.java
@@ -2,7 +2,10 @@ package com.bp.msdevops.rest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +34,20 @@ public class DevOpsController {
         } catch (Exception e) {
             return manejoExcepciones.registraLogExcepcionRest(e, EstadoErrorEnum.EJECUCION.getCode());
         }
+    }
+
+    @GetMapping()
+    public String sendMessageErrorGet() {
+        return "ERROR";
+    }
+
+    @PutMapping()
+    public String sendMessageErrorPut() {
+        return "ERROR";
+    }
+
+    @DeleteMapping()
+    public String sendMessageErrorDelete() {
+        return "ERROR";
     }
 }
